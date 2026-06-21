f0ed6014b59477c5078a576666deb93590817ab9
neursh <31346263+neursh@users.noreply.github.com>
2026-06-21 10:03:40 +0700

    Add a koii prefix

--- dev/database.sh
+++ dev/database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 identifier="#Koii's managed hosts"
-hosts="127.0.0.1 mongo1 mongo2 mongo3"
+hosts="127.0.0.1 koiiMongoPrimary koiiMongo2 koiiMongo3"
 networks=$(podman network ls)
 
 if [[ "$networks" != *"koiiMongodbCluster"* ]]; then
@@ -10,51 +10,51 @@ fi
 if [[ "$1" == "remove" ]]; then
   sudo sed -i "\#^${hosts}\$#d" "/etc/hosts"
   sudo -k
-  podman exec -it mongo2 mongosh --eval 'db.shutdownServer({ force: true })'
-  podman exec -it mongo3 mongosh --eval 'db.shutdownServer({ force: true })'
-  podman exec -it mongo1 mongosh --eval 'db.shutdownServer({ force: true })'
-  podman rm -f mongo1
-  podman rm -f mongo2
-  podman rm -f mongo3
-  podman rm -f dragonfly1
+  podman exec -it koiiMongo2 mongosh --eval 'db.shutdownServer({ force: true })'
+  podman exec -it koiiMongo3 mongosh --eval 'db.shutdownServer({ force: true })'
+  podman exec -it koiiMongoPrimary mongosh --eval 'db.shutdownServer({ force: true })'
+  podman rm -f koiiMongoPrimary
+  podman rm -f koiiMongo2
+  podman rm -f koiiMongo3
+  podman rm -f koiiDragonfly
   exit 0
 fi
 
 if [[ "$1" == "down" ]]; then
   sudo sed -i "\#^${hosts}\$#d" "/etc/hosts"
   sudo -k
-  podman exec -it mongo1 mongosh --eval 'db.shutdownServer({ force: true })'
-  podman exec -it mongo2 mongosh --eval 'db.shutdownServer({ force: true })'
-  podman exec -it mongo3 mongosh --eval 'db.shutdownServer({ force: true })'
-  podman stop mongo1
-  podman stop mongo2
-  podman stop mongo3
-  podman stop dragonfly1
+  podman exec -it koiiMongoPrimary mongosh --eval 'db.shutdownServer({ force: true })'
+  podman exec -it koiiMongo2 mongosh --eval 'db.shutdownServer({ force: true })'
+  podman exec -it koiiMongo3 mongosh --eval 'db.shutdownServer({ force: true })'
+  podman stop koiiMongoPrimary
+  podman stop koiiMongo2
+  podman stop koiiMongo3
+  podman stop koiiDragonfly
   exit 0
 fi
 
 sudo sed -i "\#^${hosts}\$#d" "/etc/hosts"
 sudo -k
 
-podman run -d -p 27017:27017 --name mongo1 --network koiiMongodbCluster mongo:8.0.4 mongod --replSet koiiReplicaSet --bind_ip localhost,mongo1
-podman run -d -p 27018:27017 --name mongo2 --network koiiMongodbCluster mongo:8.0.4 mongod --replSet koiiReplicaSet --bind_ip localhost,mongo2
-podman run -d -p 27019:27017 --name mongo3 --network koiiMongodbCluster mongo:8.0.4 mongod --replSet koiiReplicaSet --bind_ip localhost,mongo3
-podman run -d -p 6379:6379 --name dragonfly1 --ulimit memlock=-1 docker.dragonflydb.io/dragonflydb/dragonfly
+podman run -d -p 27017:27017 --name koiiMongoPrimary --network koiiMongodbCluster mongo:8.0.4 mongod --replSet koiiReplicaSet --bind_ip localhost,koiiMongoPrimary
+podman run -d -p 27018:27017 --name koiiMongo2 --network koiiMongodbCluster mongo:8.0.4 mongod --replSet koiiReplicaSet --bind_ip localhost,koiiMongo2
+podman run -d -p 27019:27017 --name koiiMongo3 --network koiiMongodbCluster mongo:8.0.4 mongod --replSet koiiReplicaSet --bind_ip localhost,koiiMongo3
+podman run -d -p 6379:6379 --name koiiDragonfly --ulimit memlock=-1 docker.dragonflydb.io/dragonflydb/dragonfly
 
 sleep 1
 
-podman exec -it mongo1 mongosh --eval "rs.initiate({
+podman exec -it koiiMongoPrimary mongosh --eval "rs.initiate({
   _id: \"koiiReplicaSet\",
   members: [
-    {_id: 0, host: \"mongo1\"},
-    {_id: 1, host: \"mongo2\"},
-    {_id: 2, host: \"mongo3\"}
+    {_id: 0, host: \"koiiMongoPrimary\"},
+    {_id: 1, host: \"koiiMongo2\"},
+    {_id: 2, host: \"koiiMongo3\"}
   ]
 })"
 
 sleep 1
 
-podman exec -it mongo2 mongosh --eval "rs.status()"
+podman exec -it koiiMongo2 mongosh --eval "rs.status()"
 
 if grep -qF "$hosts" "/etc/hosts"; then
   echo "Skipping hosts write."
